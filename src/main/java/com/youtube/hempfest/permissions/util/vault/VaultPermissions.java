3319b62cd5661b12b26fd8aa997383d3d375e3a7
Hempfest <64885225+Hempfest@users.noreply.github.com>
2021-01-18 19:32:42 -0800

    * Well then.. Memory leak solved. No more runnable.

--- src/main/java/com/youtube/hempfest/permissions/util/vault/VaultPermissions.java
+++ src/main/java/com/youtube/hempfest/permissions/util/vault/VaultPermissions.java
@@ -7,7 +7,9 @@ import net.milkbowl.vault.permission.Permission;
 import org.bukkit.OfflinePlayer;
 
 public class VaultPermissions extends Permission {
-    
+
+    private final PermissionHook listener = HempfestPermissions.getInstance().listener;
+
     @Override
     public String getName() {
         return HempfestPermissions.getInstance().getDescription().getName();
@@ -30,122 +32,103 @@ public class VaultPermissions extends Permission {
 
     @Override
     public boolean playerHas(String world, OfflinePlayer player, String permission) {
-        PermissionHook listener = new PermissionHook();
+
         return listener.playerHas(player, world, permission);
     }
 
     @Override
     public boolean playerAdd(String world, String playerName, String permission) {
-        PermissionHook listener = new PermissionHook();
         return true;
     }
 
     @Override
     public boolean playerAdd(String world, OfflinePlayer player, String permission) {
-        PermissionHook listener = new PermissionHook();
         listener.playerGive(player, world, permission);
         return true;
     }
 
     @Override
     public boolean playerRemove(String world, String playerName, String permission) {
-        PermissionHook listener = new PermissionHook();
         return false;
     }
 
     @Override
     public boolean playerRemove(String world, OfflinePlayer player, String permission) {
-        PermissionHook listener = new PermissionHook();
         listener.playerTake(player, world, permission);
         return false;
     }
 
     @Override
     public boolean groupHas(String world, String group, String permission) {
-        PermissionHook listener = new PermissionHook();
         return listener.groupHas(group, world, permission);
     }
 
     @Override
     public boolean groupAdd(String world, String group, String permission) {
-        PermissionHook listener = new PermissionHook();
         listener.groupGive(group, world, permission);
         return true;
     }
 
     @Override
     public boolean groupRemove(String world, String group, String permission) {
-        PermissionHook listener = new PermissionHook();
         listener.groupTake(group, world, permission);
         return true;
     }
 
     @Override
     public boolean playerInGroup(String world, String playerName, String group) {
-        PermissionHook listener = new PermissionHook();
         return false;
     }
 
     @Override
     public boolean playerInGroup(String world, OfflinePlayer player, String group) {
-        PermissionHook listener = new PermissionHook();
         return listener.playerInGroup(player, world, group);
     }
 
     @Override
     public boolean playerAddGroup(String world, String playerName, String group) {
-        PermissionHook listener = new PermissionHook();
         return false;
     }
 
     @Override
     public boolean playerAddGroup(String world, OfflinePlayer player, String group) {
-        PermissionHook listener = new PermissionHook();
         listener.playerGiveGroup(player, world, group);
         return true;
     }
 
     @Override
     public boolean playerRemoveGroup(String world, String playerName, String group) {
-        PermissionHook listener = new PermissionHook();
         return false;
     }
 
     @Override
     public boolean playerRemoveGroup(String world, OfflinePlayer player, String group) {
-        PermissionHook listener = new PermissionHook();
         listener.playerTakeGroup(player,world, group);
         return true;
     }
 
     @Override
     public String[] getPlayerGroups(String world, String playerName) {
-        PermissionHook listener = new PermissionHook();
         return new String[0];
     }
 
     @Override
     public String[] getPlayerGroups(String world, OfflinePlayer player) {
-        PermissionHook listener = new PermissionHook();
         return listener.getGroups(player, world);
     }
 
     @Override
     public String getPrimaryGroup(String world, OfflinePlayer player) {
-        UtilityManager um = new UtilityManager();
-        PermissionHook listener = new PermissionHook();
         return listener.getGroup(player, world);
     }
 
     @Override
     public String getPrimaryGroup(String world, String playerName) {
-        PermissionHook listener = new PermissionHook();
         return null;
     }
 
     @Override
     public String[] getGroups() {
-        PermissionHook listener = new PermissionHook();
         return listener.getAllGroups();
     }
 
