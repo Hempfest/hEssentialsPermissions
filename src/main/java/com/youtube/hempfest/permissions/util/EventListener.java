206723bc25ea703edd6223da502c8eeef4bf0b4d
Hempfest <64885225+Hempfest@users.noreply.github.com>
2021-01-18 16:26:06 -0800

    * Well then.. Memory leak solved. No more runnable.

--- src/main/java/com/youtube/hempfest/permissions/util/EventListener.java
+++ src/main/java/com/youtube/hempfest/permissions/util/EventListener.java
@@ -2,13 +2,16 @@ package com.youtube.hempfest.permissions.util;
 
 import com.youtube.hempfest.permissions.HempfestPermissions;
 import com.youtube.hempfest.permissions.util.events.PermissionUpdateEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class EventListener implements Listener {
 
@@ -25,6 +28,15 @@ public class EventListener implements Listener {
         Player p = e.getPlayer();
         HempfestPermissions.getInstance().playerPermissions.remove(p.getUniqueId());
     }
+
+    @EventHandler
+    public void onTeleport(PlayerChangedWorldEvent e) {
+            PermissionUpdateEvent event = new PermissionUpdateEvent();
+            Bukkit.getPluginManager().callEvent(event);
+            if (!event.isCancelled()) {
+                event.query();
+            }
+        }
 
     @EventHandler
     public void logActions(PermissionUpdateEvent e) {
